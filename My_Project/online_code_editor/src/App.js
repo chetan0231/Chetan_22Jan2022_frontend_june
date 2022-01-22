4e2c00214cea78e68f5ca40e2e589ff94be34fdb
chetan0231 <DK0231@digikull.com>
2022-01-22 19:29:25 +0530

    files added successfully

---
+++ My_Project/online_code_editor/src/App.js
@@ -0,0 +1,31 @@
+import React,{useState} from "react";
+import Editor from "./components/Editor";
+
+function App() {
+  const [html,setHtml] = useState('')
+  const [css,setCss] = useState('')
+  const [js,setJs] = useState('')
+
+  return (
+    <div className="App">
+      <div className="pane top-pane">
+        <Editor language="xml" displayName="HTML" value={html} onChange={setHtml}/>  
+        <Editor language="css" displayName="CSS" value={css} onChange={setCss}/>
+        <Editor language="javasript" displayName="JS" value={js} onChange={setJs}/>
+      </div>
+      <div className="bottom-pane">
+        <iframe
+          // srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder = "0"
+          width="100%"
+          height="100%"
+        ></iframe>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ My_Project/online_code_editor/src/components/Editor.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/css/css";
+import { UnControlled } from "react-codemirror2";
+
+const Editor = (props) => {
+  const { displayName, language,value,onChange } = props;
+
+  function handleChange(editor,data,value){
+    onChange(value)
+  }
+
+  return (
+    <div className="editor-container">
+        <div className="editor-title">
+            {displayName}
+            <button>X</button>
+        </div>
+      <UnControlled
+        onBeforeChange={handleChange}
+        value={value}
+        className="code-mirror-wrapper"
+        options={{
+          lineWrapping: true,
+          lint: true,
+          mode: language,
+          theme : 'material',
+          lineNumbers : true
+        }}
+      />
+    </div>
+  );
+};
+
+export default Editor;
