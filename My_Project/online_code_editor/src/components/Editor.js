bd4b073f3b770112a22a4746bb82f6ff5b7f2d65
chetan0231 <DK0231@digikull.com>
2022-01-24 20:07:41 +0530

    changes made

--- My_Project/online_code_editor/src/components/Editor.js
+++ My_Project/online_code_editor/src/components/Editor.js
@@ -1,25 +1,28 @@
-import React from "react";
+import React,{useState} from "react";
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
 import 'codemirror/mode/xml/xml';
 import "codemirror/mode/javascript/javascript";
 import 'codemirror/mode/css/css';
-import { UnControlled } from "react-codemirror2";
+import { Controlled } from "react-codemirror2";
+
 
 const Editor = (props) => {
   const { displayName, language, value, onChange } = props;
+  const [open,setOpen] = useState(true)
+
 
   function handleChange(editor, data, value) {
     onChange(value);
   }
 
   return (
-    <div className="editor-container">
+    <div className={`editor-container ${open ? '' : 'collapsed'}`}>
       <div className="editor-title">
         {displayName}
         <button>X</button>
       </div>
-      <UnControlled
+      <Controlled
         onBeforeChange={handleChange}
         value={value}
         className="code-mirror-wrapper"
